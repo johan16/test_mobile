954d3dfe0119ebf585a77695e34bc98a2a4d52d0
maurobahamondes@gmail.com <maurobahamondes@gmail.com>
2013-08-29 10:53:42 -0400

    Guardar en la BDD

--- db.js
+++ db.js
@@ -1,59 +1,104 @@
-// JavaScript Document
-
-//function init() {
-	//alert('1');
-	//document.addEventListener("deviceready", deviceready, true);
-//}
-
-$(document).on('deviceready', deviceready);
-
+// global variables
 var db;
-
-function deviceready() {
-	alert('2');
-	db = window.openDatabase("test", "1.0", "test", 1000000);
-	db.transaction(setup, errorHandler, dbReady);
+var shortName = 'WebSqlDB';
+var version = '1.0';
+var displayName = 'WebSqlDB';
+var maxSize = 65535;
+ 
+// this is called when an error happens in a transaction
+function errorHandler(transaction, error) {
+   alert('Error: ' + error.message + ' code: ' + error.code);
+ 
 }
-
-function setup(tx) {
-	alert('3');
-	tx.executeSql('create table if not exists euro(id INTEGER PRIMARY KEY AUTOINCREMENT, q1 TEXT, q2 TEXT, created DATE)');
+ 
+// this is called when a successful transaction happens
+function successCallBack() {
+   alert("DEBUGGING: success");
+ 
 }
-
-function errorHandler(e) {
-	alert(e.message);
+ 
+function nullHandler(){};
+ 
+// called when the application loads
+function onBodyLoad(){
+ 
+// This alert is used to make sure the application is loaded correctly
+// you can comment this out once you have the application working
+alert("DEBUGGING: we are in the onBodyLoad() function");
+ 
+ if (!window.openDatabase) {
+   // not all mobile devices support databases  if it does not, the following alert will display
+   // indicating the device will not be albe to run this application
+   alert('Databases are not supported in this browser.');
+   return;
+ }
+ 
+// this line tries to open the database base locally on the device
+// if it does not exist, it will create it and return a database object stored in variable db
+ db = openDatabase(shortName, version, displayName,maxSize);
+ 
+// this line will try to create the table User in the database just created/openned
+ db.transaction(function(tx){
+ 
+  // you can uncomment this next line if you want the User table to be empty each time the application runs
+  // tx.executeSql( 'DROP TABLE User',nullHandler,nullHandler);
+ 
+  // this line actually creates the table User if it does not exist and sets up the three columns and their types
+  // note the UserId column is an auto incrementing column which is useful if you want to pull back distinct rows
+  // easily from the table.
+   tx.executeSql( 'CREATE TABLE IF NOT EXISTS User(UserId INTEGER NOT NULL PRIMARY KEY, FirstName TEXT NOT NULL, LastName TEXT NOT NULL)',
+[],nullHandler,errorHandler);
+ },errorHandler,successCallBack);
+ 
 }
-
-function dbReady() {
-	$("#addButton").on("click", function(e){
-		alert('4');
-		db.transaction(function(tx) {
-			var q1 = "prueba";
-			var q2 = "prueba2";
-			tx.executeSQL("insert into euro(q1,q2) values('?,?')",[q1,q2]);
-		}, errorHandler, function() { $("#result").html("Guardado."); });
-	});
-
-	$("#consultar").on("click", function(e){
-		alert('5');
-		db.transaction(function(tx) {
-			tx.executeSql("select * from euro order by created desc", [], gotLog,errorHandler);
-		},errorHandler, function() {});
-	});
+ 
+// list the values in the database to the screen using jquery to update the #lbUsers element
+function ListDBValues() {
+ alert("list refresh");
+ if (!window.openDatabase) {
+  alert('Databases are not supported in this browser.');
+  return;
+ }
+ 
+// this line clears out any content in the #lbUsers element on the page so that the next few lines will show updated
+// content and not just keep repeating lines
+ $('#lbUsers').html('');
+ 
+// this next section will select all the content from the User table and then go through it row by row
+// appending the UserId  FirstName  LastName to the  #lbUsers element on the page
+ db.transaction(function(transaction) {
+   transaction.executeSql('SELECT * FROM User;', [],
+     function(transaction, result) {
+      if (result != null && result.rows != null) {
+        for (var i = 0; i < result.rows.length; i++) {
+          var row = result.rows.item(i);
+          $('#lbUsers').append('<br>' + row.UserId + '. ' + row.FirstName+ ' ' + row.LastName);
+        }
+      }
+     },errorHandler);
+ },errorHandler,nullHandler);
+ 
+ return;
+ 
 }
-
-function gotLog(tx, results) {
-	if(results.rows.length == 0) {
-		$("#result").html("Sin datos.");
-		return false;
-	}
-
-	var s = "";
-	for(var i=0; i<results.rows.length; i++) {
-		var d = new Date();
-		d.setTime(results.rows.item(i).created);
-		s += d.toDateString() + " "+d.toTimeString() + "<br />";
-	}
+ 
+// this is the function that puts values into the database using the values from the text boxes on the screen
+function AddValueToDB() {
+ alert("click add value");
+ if (!window.openDatabase) {
+   alert('Databases are not supported in this browser.');
+   return;
+ }
+ 
+// this is the section that actually inserts the values into the User table
+ db.transaction(function(transaction) {
+   transaction.executeSql('INSERT INTO User(FirstName, LastName) VALUES (?,?)',[$('#txFirstName').val(), $('#txLastName').val()],
+     nullHandler,errorHandler);
+   });
+ 
+// this calls the function that will show what is in the User table in the database
+ ListDBValues();
+ 
+ return false;
+ 
 }
-
-$("#results").html(s);
