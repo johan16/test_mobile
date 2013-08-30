d2402f372310a879aab141aaea64cfb7a13db3fb
maurobahamondes@gmail.com <maurobahamondes@gmail.com>
2013-08-30 11:29:48 -0400

    First page

--- db.js
+++ db.js
@@ -8,13 +8,11 @@ var maxSize = 65535;
 // this is called when an error happens in a transaction
 function errorHandler(transaction, error) {
    alert('Error: ' + error.message + ' code: ' + error.code);
- 
 }
  
 // this is called when a successful transaction happens
 function successCallBack() {
    //alert("DEBUGGING: success");
- 
 }
  
 function nullHandler(){};
@@ -22,64 +20,63 @@ function nullHandler(){};
 // called when the application loads
 function onBodyLoad(){
  
-// This alert is used to make sure the application is loaded correctly
-// you can comment this out once you have the application working
-//alert("DEBUGGING: we are in the onBodyLoad() function");
- 
- if (!window.openDatabase) {
-   // not all mobile devices support databases  if it does not, the following alert will display
-   // indicating the device will not be albe to run this application
-   alert('Databases are not supported in this browser.');
-   return;
- }
- 
-// this line tries to open the database base locally on the device
-// if it does not exist, it will create it and return a database object stored in variable db
- db = openDatabase(shortName, version, displayName,maxSize);
- 
-// this line will try to create the table User in the database just created/openned
- db.transaction(function(tx){
- 
-  // you can uncomment this next line if you want the User table to be empty each time the application runs
-  // tx.executeSql( 'DROP TABLE User',nullHandler,nullHandler);
- 
-  // this line actually creates the table User if it does not exist and sets up the three columns and their types
-  // note the UserId column is an auto incrementing column which is useful if you want to pull back distinct rows
-  // easily from the table.
-   tx.executeSql( 'CREATE TABLE IF NOT EXISTS User(Id INTEGER NOT NULL PRIMARY KEY, pregunta TEXT NOT NULL, foto TEXT NOT NULL)',
-[],nullHandler,errorHandler);
- },errorHandler,successCallBack);
+    // This alert is used to make sure the application is loaded correctly
+    // you can comment this out once you have the application working
+    alert("DEBUGGING: we are in the onBodyLoad() function");
+
+    if (!window.openDatabase) {
+     // not all mobile devices support databases  if it does not, the following alert will display
+     // indicating the device will not be albe to run this application
+     alert('Databases are not supported in this browser.');
+     return;
+    }
+
+    // this line tries to open the database base locally on the device
+    // if it does not exist, it will create it and return a database object stored in variable db
+    db = openDatabase(shortName, version, displayName,maxSize);
+
+    // this line will try to create the table User in the database just created/openned
+    db.transaction(function(tx){
+
+    // you can uncomment this next line if you want the User table to be empty each time the application runs
+    // tx.executeSql( 'DROP TABLE User',nullHandler,nullHandler);
+
+    // this line actually creates the table User if it does not exist and sets up the three columns and their types
+    // note the UserId column is an auto incrementing column which is useful if you want to pull back distinct rows
+    // easily from the table.
+    tx.executeSql( 'CREATE TABLE IF NOT EXISTS User(Id INTEGER NOT NULL PRIMARY KEY, pregunta TEXT NOT NULL, foto TEXT NOT NULL)',
+      [],nullHandler,errorHandler);
+    },errorHandler,successCallBack);
  
 }
  
 // list the values in the database to the screen using jquery to update the #lbUsers element
 function ListDBValues() {
- alert("list refresh");
- if (!window.openDatabase) {
-  alert('Databases are not supported in this browser.');
-  return;
- }
+    alert("list refresh");
+    if (!window.openDatabase) {
+      alert('Databases are not supported in this browser.');
+      return;
+    }
  
-// this line clears out any content in the #lbUsers element on the page so that the next few lines will show updated
-// content and not just keep repeating lines
- $('#lbUsers').html('');
+    // this line clears out any content in the #lbUsers element on the page so that the next few lines will show updated
+    // content and not just keep repeating lines
+    $('#lbUsers').html('');
  
-// this next section will select all the content from the User table and then go through it row by row
-// appending the UserId  FirstName  LastName to the  #lbUsers element on the page
- db.transaction(function(transaction) {
-   transaction.executeSql('SELECT * FROM User;', [],
-     function(transaction, result) {
-      if (result != null && result.rows != null) {
-        for (var i = 0; i < result.rows.length; i++) {
-          var row = result.rows.item(i);
-          $('#lbUsers').append('<br>' + row.Id + '. ' + row.pregunta+ ' ' + row.foto);
+    // this next section will select all the content from the User table and then go through it row by row
+    // appending the UserId  FirstName  LastName to the  #lbUsers element on the page
+    db.transaction(function(transaction) {
+     transaction.executeSql('SELECT * FROM User;', [],
+       function(transaction, result) {
+        if (result != null && result.rows != null) {
+          for (var i = 0; i < result.rows.length; i++) {
+            var row = result.rows.item(i);
+            $('#lbUsers').append('<br>' + row.Id + '. ' + row.pregunta+ ' ' + row.foto);
+          }
         }
-      }
-     },errorHandler);
- },errorHandler,nullHandler);
- 
- return;
- 
+       },errorHandler);
+    },errorHandler,nullHandler);
+
+    return;
 }
  
 // this is the function that puts values into the database using the values from the text boxes on the screen
@@ -87,8 +84,8 @@ function AddValueToDB() {
     var q1 = $('#checkbox-1a').val();
     var q2 = $('#lafoto').val();
     if (!window.openDatabase) {
-     alert('Databases are not supported in this browser.');
-     return;
+      alert('Databases are not supported in this browser.');
+      return;
     }
   
     // this is the section that actually inserts the values into the User table
@@ -99,9 +96,6 @@ function AddValueToDB() {
     // this calls the function that will show what is in the User table in the database
     //pictureSource=navigator.camera.PictureSourceType; 
     //destinationType=navigator.camera.DestinationType;
-
-    // window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, onFileSystemSuccess, fail);
-    // window.resolveLocalFileSystemURI(q2, uploadPhoto, fail);
 
     uploadPhoto(q2);
     ListDBValues();
@@ -120,34 +114,6 @@ function fail(evt) {
     alert(evt.target.error.code);
 }
 
-//function getPhoto(source) {
-
-// Retrieve image file location from specified source 
-//navigator.camera.getPicture(onPhotoURISuccess, onFail, {
-//quality: 50, destinationType: destinationType.FILE_URI, sourceType: source });
-
-//}
-
-//function onDeviceReady() {
-//        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, onFileSystemSuccess, fail);
-//        window.resolveLocalFileSystemURI("file:///example.txt", onResolveSuccess, fail);
-//    }
-//
-//function onFileSystemSuccess(fileSystem) {
-//		alert("1" + fileSystem.name);
-//        console.log(fileSystem.name);
-//    }
-//
-//    function onResolveSuccess(fileEntry) {
-//		alert("2" + fileEntry.name);
-//        console.log(fileEntry.name);
-//    }
-//
-//    function fail(evt) {
-//		alert("3" + evt.target.error.code);
-//        console.log(evt.target.error.code);
-//    }
-
 
 function uploadPhoto(imageURI) {
     alert('1');
@@ -156,7 +122,7 @@ function uploadPhoto(imageURI) {
     options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
     options.mimeType="image/jpeg";
 
-    var params = new Object();
+    var params = {};
     params.value1 = "test";
     params.value2 = "param";
 
@@ -166,16 +132,16 @@ function uploadPhoto(imageURI) {
     ft.upload(imageURI, "http://www.eurofashion.cl/app_euro/", win, fail, options);
 }	
 	
-	function win(r) {
-			alert('3');
-            console.log("Code = " + r.responseCode);
-            console.log("Response = " + r.response);
-            console.log("Sent = " + r.bytesSent);
-    }
+function win(r) {
+    alert('3');
+    console.log("Code = " + r.responseCode);
+    console.log("Response = " + r.response);
+    console.log("Sent = " + r.bytesSent);
+}
 
-    function fail(error) {
-			alert('4');
-            alert("An error has occurred: Code = " + error.code);
-            console.log("upload error source " + error.source);
-            console.log("upload error target " + error.target);
-    }
+function fail(error) {
+    alert('4');
+    alert("An error has occurred: Code = " + error.code);
+    console.log("upload error source " + error.source);
+    console.log("upload error target " + error.target);
+}
---
+++ index.js
@@ -0,0 +1,63 @@
+$(document).on('deviceready', onDeviceReady);
+function onDeviceReady(){
+	$('#btnCamera').on('click', function() {
+		sessionStorage.removeItem('imagepath');
+		navigator.camera.getPicture(onSuccessCamera, onErrorCamera, {destinationType : Camera.DestinationType.FILE_URI , 
+															        sourceType : Camera.PictureSourceType.CAMERA,
+															        allowEdit : false,
+																    targetWidth:  1024,
+															        targetHeight: 1024,
+															    	quality : 100,
+															    	correctOrientation : true,
+															    	saveToPhotoAlbum : true});
+											
+	});
+
+	onBodyLoad();
+}
+
+function onSuccessCamera(imageData){
+	var imagen = $('.capture > img');
+		imagen.attr({ 'src': imageData, "width" : "320px"} );
+		imagen.css( 'display', 'block');
+		movePic(imageData);
+}
+
+function onErrorCamera(message){
+	console.log("Error:" + message);
+}
+
+function movePic(file){ 
+    window.resolveLocalFileSystemURI(file, resolveOnSuccess, resOnError); 
+} 
+
+//Callback function when the file system uri has been resolved
+function resolveOnSuccess(entry){ 
+    var d = new Date();
+    var n = d.getTime();
+    //new file name
+    var newFileName = n + ".jpg";
+    var myFolderApp = "Eurofashion";
+
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, 
+    	function(fileSys) {      
+		    //The folder is created if doesn't exist
+		    fileSys.root.getDirectory( myFolderApp, {create:true, exclusive: false}, 
+		    	function(directory) {
+										entry.moveTo(directory, newFileName,  successMove, resOnError);
+									}, resOnError);
+        },
+    resOnError);
+}
+
+//Callback function when the file has been moved successfully - inserting the complete path
+function successMove(entry) {
+    //Store imagepath in session for future use
+    // like to store it in database
+    sessionStorage.setItem('imagepath', entry.fullPath);
+    $("#lafoto").val(entry.fullPath);	
+}
+
+function resOnError(error) {
+    alert(error.code);
+}
